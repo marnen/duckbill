5f92f7e918f68fe8bc88ff20b535b5bcec51b797
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-26 15:05:23 -0500

    Use a password that Devise won't reject. [#86853004]

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password do
-      1.upto(rand 4..20).collect do
+      1.upto(rand 8..20).collect do
         (Faker::String::WORD_CHARS + Faker::String::SPACES).shuffle.first
       end.join
     end
