882d2d6a99fd64d68911e9a340eee0063a2795b4
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-26 16:47:59 -0500

    Remove deprecation. [#86853004]

--- features/step_definitions/time_entry_steps.rb
+++ features/step_definitions/time_entry_steps.rb
@@ -5,7 +5,7 @@ end
 Then 'I should see a time entry for:' do |table|
   within '.time_entry' do
     table.rows_hash.each do |field, value|
-      page.should have_selector ".#{field.delete(' ').underscore}", text: value
+      expect(page).to have_selector ".#{field.delete(' ').underscore}", text: value
     end
   end
 end
