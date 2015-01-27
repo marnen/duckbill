9b2ebafafe8927cdc4b4dcf8134ee1bf6e1c7400
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-26 21:37:01 -0500

    rake gettext:find gettext:store_model_attributes [#86853004]

---
+++ model_attributes.rb
@@ -0,0 +1,18 @@
+#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!
+_('Time entry')
+_('TimeEntry|Date')
+_('TimeEntry|Description')
+_('TimeEntry|Hours')
+_('TimeEntry|Notes')
+_('User')
+_('User|Current sign in at')
+_('User|Current sign in ip')
+_('User|Email')
+_('User|Encrypted password')
+_('User|Last sign in at')
+_('User|Last sign in ip')
+_('User|Remember created at')
+_('User|Reset password sent at')
+_('User|Reset password token')
+_('User|Sign in count')
+#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!
