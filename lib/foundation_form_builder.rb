6cd7572dd5e6011f66496f7823b42c9e9cacdedc
Marnen Laibow-Koser <marnen@marnen.org>
2015-02-02 20:59:48 -0500

    Allow customization of label text. [#87249152]

--- lib/foundation_form_builder.rb
+++ lib/foundation_form_builder.rb
@@ -1,8 +1,8 @@
 class FoundationFormBuilder < ActionView::Helpers::FormBuilder
-  def input_div(field_name)
+  def input_div(field_name, label: nil)
     @template.content_tag :div, class: field_name do
       [
-        label(field_name),
+        label(field_name, label),
         input_for(field_name),
         error_div(field_name)
       ].compact.join("\n").html_safe
@@ -12,7 +12,7 @@ class FoundationFormBuilder < ActionView::Helpers::FormBuilder
   private
 
   def error_div(field_name)
-    if error_message_on(field_name).present?
+    if error_message_on(field_name).present? # TODO: do we really need dynamic_form just for this?
       @template.content_tag :div, class: :error do
         error_message_on field_name
       end
--- spec/lib/foundation_form_builder_spec.rb
+++ spec/lib/foundation_form_builder_spec.rb
@@ -13,20 +13,13 @@ describe FoundationFormBuilder, type: :view do
     let(:input_id) { [object_name, field_name].join '_' }
     let(:input_name) { "#{object_name}[#{field_name}]" }
     let(:wrapper) { "div.#{field_name}" }
-    subject(:input_div) { builder.input_div field_name }
+    let(:options) { {} }
+    subject(:input_div) { builder.input_div field_name, options }
 
     it { should be_html_safe }
 
     it 'wraps the whole thing in a div with a class named for the field' do
       expect(input_div).to have_tag wrapper, count: 1
-    end
-
-    it 'renders a label within the wrapper' do
-      expect(input_div).to have_tag "#{wrapper} label[for='#{input_id}']", text: field_name.to_s.humanize
-    end
-
-    it 'renders the field within the wrapper' do
-      expect(input_div).to have_tag "#{wrapper} input##{input_id}[type='text'][name='#{input_name}']"
     end
 
     describe 'errors' do
@@ -45,6 +38,23 @@ describe FoundationFormBuilder, type: :view do
 
         it 'renders an error element' do
           expect(input_div).to have_tag "#{wrapper} .error", text: error_message
+        end
+      end
+    end
+
+    describe 'label' do
+      context 'label text specified' do
+        let(:label_text) { Faker::Lorem.sentence }
+        let(:options) { {label: label_text} }
+
+        it 'uses the specified text for the label' do
+          expect(input_div).to have_tag "#{wrapper} label[for='#{input_id}']", text: label_text
+        end
+      end
+
+      context 'otherwise' do
+        it 'uses the humanized field name as the label text' do
+          expect(input_div).to have_tag "#{wrapper} label[for='#{input_id}']", text: field_name.to_s.humanize
         end
       end
     end
