070091cfb5eddc009121a436d6a14b0a3b859ea2
Marnen Laibow-Koser <marnen@marnen.org>
2015-02-04 11:08:13 -0500

    Remove Devise helpers from cell. [#87241770]

--- app/cells/client_selector_cell.rb
+++ app/cells/client_selector_cell.rb
@@ -1,10 +1,8 @@
 class ClientSelectorCell < Cell::Rails
-  include Devise::Controllers::Helpers
-  helper_method :current_user
-
   def show(args)
     @form = args[:form]
-    @clients = current_user.clients
+    user = args[:user]
+    @clients = user.clients
 
     render
   end
