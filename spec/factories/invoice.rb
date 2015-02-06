01a1e53a61ef852fa115da31cc752bfb32e1f1f2
Marnen Laibow-Koser <marnen@marnen.org>
2015-02-06 18:48:19 -0500

    Render invoice as PDF file. [#87775500]

--- spec/factories/invoice.rb
+++ spec/factories/invoice.rb
@@ -1,5 +1,16 @@
 FactoryGirl.define do
   factory :invoice do
+    transient do
+      user nil
+    end
+
     project
+
+    after(:build) do |invoice, evaluator|
+      user = evaluator.user
+      if user
+        invoice.project = FactoryGirl.create :project, user: user
+      end
+    end
   end
 end
