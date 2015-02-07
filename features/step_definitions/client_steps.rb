7704dd1c5bf5f6ef9d25888f09c836ab3e96f5e8
Marnen Laibow-Koser <marnen@marnen.org>
2015-02-07 16:53:17 -0500

    Move factory steps to one file for ease of refactoring. [#87860360]

--- features/step_definitions/client_steps.rb
+++ features/step_definitions/client_steps.rb
@@ -1,25 +1,3 @@
-Given 'I have no clients' do
-  @current_user.clients.destroy_all
-end
-
-Given /^I have (\d+) clients?$/ do |count|
-  count.times do
-    FactoryGirl.create :client, user: @current_user
-  end
-end
-
-Given 'the following client exists:' do |table|
-  params = params_from table
-  params['user'] = User.find_by_email(params['user']) if params.include? 'user'
-  @client = FactoryGirl.create :client, params
-end
-
-Given 'I have the following client:' do |table|
-  params = params_from(table).merge user: @current_user
-  @client = FactoryGirl.create :client, :with_name_and_address, params
-end
-
-
 Then /^I should (not )?see the following client:$/ do |negation, table|
   within '.client' do
     table.rows_hash.each do |field, value|
---
+++ features/step_definitions/factory_steps.rb
@@ -0,0 +1,80 @@
+Given 'I have no clients' do
+  @current_user.clients.destroy_all
+end
+
+Given /^I have (\d+) clients?$/ do |count|
+  count.times do
+    FactoryGirl.create :client, user: @current_user
+  end
+end
+
+Given 'the following client exists:' do |table|
+  params = params_from table
+  params['user'] = User.find_by_email(params['user']) if params.include? 'user'
+  @client = FactoryGirl.create :client, params
+end
+
+Given 'I have the following client:' do |table|
+  params = params_from(table).merge user: @current_user
+  @client = FactoryGirl.create :client, :with_name_and_address, params
+end
+
+Given 'I have an invoice' do
+  @invoice = FactoryGirl.create :invoice, user: @current_user
+end
+
+Given 'I have the following invoice:' do |table|
+  hash = table.hashes.first
+  if hash['project']
+    hash['project'] = @current_user.projects.find_by_name(hash['project']) || FactoryGirl.create(:project, user: @current_user, name: hash['project'])
+  else
+    hash['user'] = @current_user
+  end
+  @invoice = FactoryGirl.create :invoice, hash
+end
+
+Given 'I have no projects' do
+  @current_user.projects.destroy_all
+end
+
+Given 'I have the following project:' do |table|
+  params = params_from table
+  @project = FactoryGirl.create :project, params.merge(user: @current_user)
+end
+
+Given 'I have the following projects:' do |table|
+  table.hashes.each do |hash|
+    client = hash['client'] ? @current_user.clients.find_by_name(hash['client']) : FactoryGirl.create(:client, user: @current_user)
+    FactoryGirl.create :project, hash.merge('client' => client)
+  end
+end
+
+Given 'the following project exists:' do |table|
+  params = params_from table
+  params['client'] = Client.find_by_name(params['client']) if params.include? 'client'
+  @project = FactoryGirl.create :project, params
+end
+
+Given 'I have no time entries' do
+  @current_user.time_entries.destroy_all
+end
+
+Given /^I have (\d+) time entr(?:y|ies)$/ do |count|
+  count.times do
+    FactoryGirl.create :time_entry, user: @current_user
+  end
+end
+
+Given 'I have the following time entries:' do |table|
+  table.hashes.each do |hash|
+    hash['project'] = @current_user.projects.find_by_name hash['project']
+    FactoryGirl.create :time_entry, hash
+  end
+end
+
+Given 'the following time entry exists:' do |table|
+  params = table.rows_hash.transform_keys &:downcase
+  params['user'] = User.find_by_email(params['user']) if params.include? 'user'
+  @time_entry = FactoryGirl.create :time_entry, params
+end
+
