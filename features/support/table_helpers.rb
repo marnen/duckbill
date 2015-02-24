158b9b42654f2b019e3601ca08339b5f311cb1f4
Marnen Laibow-Koser <marnen@marnen.org>
2015-02-24 13:17:33 -0500

    Rename. [#87860326]

--- features/support/table_helpers.rb
+++ features/support/table_helpers.rb
@@ -1,7 +1,7 @@
-module ParamsHelper
+module TableHelpers
   def params_from(table)
     table.rows_hash.transform_keys {|key| remove_spaces key.downcase.delete '-' }
   end
 end
 
-World ParamsHelper
+World TableHelpers
