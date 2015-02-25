24479011cde74a5642a54c598dec9d40ddc70522
Marnen Laibow-Koser <marnen@marnen.org>
2015-02-25 18:47:24 -0500

    Move implementation file into gem. [#89149912]

--- foundation_form_builder/lib/foundation_form_builder/version.rb
+++ foundation_form_builder/lib/foundation_form_builder/version.rb
@@ -1,3 +1,5 @@
-module FoundationFormBuilder
-  VERSION = "0.1.0"
+require File.join(File.dirname(__FILE__), '..', 'foundation_form_builder')
+
+FoundationFormBuilder.class_eval do
+  self::VERSION = "0.1.0"
 end
--- foundation_form_builder/spec/spec_helper.rb
+++ foundation_form_builder/spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'ffaker'
-require 'action_view'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'foundation_form_builder'
