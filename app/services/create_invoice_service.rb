bb338f1a95a604e7ac483db6652bb4f9814cdd39
Marnen Laibow-Koser <marnen@marnen.org>
2015-03-23 23:11:14 -0400

    Extract scope. [#87775582]

--- app/services/create_invoice_service.rb
+++ app/services/create_invoice_service.rb
@@ -3,7 +3,7 @@ class CreateInvoiceService
     def call(invoice)
       Invoice.transaction do
         invoice.capture_association_versions!
-        invoice.save && TimeEntry.where(project_id: invoice.project_id, invoice_id: nil).update_all(invoice_id: invoice.id)
+        invoice.save && TimeEntry.uninvoiced.where(project_id: invoice.project_id).update_all(invoice_id: invoice.id)
       end
     end
   end
